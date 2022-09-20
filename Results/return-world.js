3dcc37a9ac81080e111ef571a199708959185d55
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-09-19 18:26:39 -0700

    format

--- Results/return-world.js
+++ Results/return-world.js
@@ -1,1 +1,16 @@
-"use strict";(self.webpackChunkbabel_starter=self.webpackChunkbabel_starter||[]).push([[678],{6558:function(e,t,a){a.r(t);a(7294);t.default=function(){var e=useGet((function(){return"world"}),[]);e.data,e.loading}}}]);
+"use strict"
+;(self.webpackChunkbabel_starter = self.webpackChunkbabel_starter || []).push([
+  [678],
+  {
+    6558: function (e, t, a) {
+      a.r(t)
+      a(7294)
+      t.default = function () {
+        var e = useGet(function () {
+          return "world"
+        }, [])
+        e.data, e.loading
+      }
+    },
+  },
+])
